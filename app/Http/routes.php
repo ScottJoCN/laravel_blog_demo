6a69420e844084f9cdf6150a6325a9ac8e66c97d
q262744210 <q262744210@126.com>
2017-12-06 16:17:30 +0800

    finish blog login and logout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,39 @@
 Route::get('/', function () {
     return view('index');
 });
+Route::get('login', function()
+{
+  return view('login');
+});
+Route::post('login',function(){
+	$rules = array(
+		'email' =>'required|email',
+		'password' =>'required|min:6',
+		'remember_me' =>'boolean',
+	);
+	$validator = Validator::make(Request::all(),$rules);
+	if($validator->passes()){
+		if(Auth::attempt([
+			'email' => Request::input('email'),
+			'password' => Request::input('password'),
+			'block' =>0
+		],
+		(boolean) Request::input('remember_me')))
+	{
+		return Redirect::to('home');
+	}
+	else{
+		return Redirect::to('login')->withInput()->with('message',array('type'=>'danger','content'=>'E-mail or password error'));
+	}
+	}
+	else{
+		return Redirect::to('login')->withInput()->withErrors($validator);
+	}
+});
+Route::get('home',['middleware'=> 'auth' , function(){
+	return view('home');
+}]);
+Route::get('logout',['middleware'=>'auth',function(){
+	Auth::logout();
+	return Redirect::to('/');
+}]);
